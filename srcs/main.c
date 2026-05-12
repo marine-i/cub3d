c7157fd7436a26d0504ed80213c193f0d67e8f8c
Marine <marine@LAPTOP-CVLJAFAU.localdomain>
2026-05-12 13:09:52 +0200

    feat(exec): add echap windows

--- srcs/main.c
+++ srcs/main.c
@@ -27,6 +27,25 @@ void	init_data(t_data *data)
 	data->player.dir_y = -1;
 }
 
+int	close_window(void *param)
+{
+	t_data *data;
+	data = (t_data *)param;
+	free_all(data);
+	exit(1);
+}
+
+int	key_hook(int keycode, t_data *game)
+{
+	if (keycode == 65307)
+		close_window(game);
+	// else
+	// 	handle_mov(keycode, game);
+	return (0);
+}
+
+
+
 int	main(int ac, char **av)
 {
 	t_data	data;
@@ -38,6 +57,8 @@ int	main(int ac, char **av)
 		return (free_all(&data), EXIT_FAILURE);
 	init_mlx(&data);
 	render_frame(&data);
+	mlx_hook(data.win_ptr, 2, 1, key_hook, &data);
+	mlx_hook(data.win_ptr, 17, 0, &close_window, &data);
 	mlx_loop(data.mlx_ptr);
 	free_all(&data);
 	return (EXIT_SUCCESS);
